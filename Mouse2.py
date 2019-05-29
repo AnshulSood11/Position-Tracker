6fbeef3868fc59c490466e9c9e75a5491f60c14f
Anshul Sood <33415007+AnshulSood11@users.noreply.github.com>
2019-05-29 23:23:46 +0530

    Code for reading displacement data from the mouse.

---
+++ Mouse2.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+import usb.core
+import usb.util
+from position_tracking.msg import Mouse_data
+
+md = Mouse_data()
+md.x=0.0
+md.y=0.0
+x=0.0
+y=0.0
+factor = 0.0000077244
+def register_data(event):
+        print str(event.current_real)
+        md.x=round(x,5)
+        md.x=round(x,5)
+        md.y=round(y,5)
+        x=0.0
+        y=0.0
+        rospy.loginfo(md)
+        publish.publish(md)
+
+
+if __name__=='__main__':
+	print 'in main'
+	# decimal vendor and product values
+	#dev = usb.core.find(idVendor=10077, idProduct=2982)
+	#this id is found by using the finddevices code
+	# or, uncomment the next line to search instead by the hexidecimal equivalent
+	dev = usb.core.find(idVendor=0x046d, idProduct=0xc084)
+	#dev = usb.core.find(idVendor=0x275d, idProduct=0xba6)
+	# first endpoint
+	interface = 0
+	endpoint = dev[0][(0,0)][0]
+	# if the OS kernel already claimed the device, which is most likely true
+	# thanks to http://stackoverflow.com/questions/8218683/pyusb-cannot-set-configuration
+	if dev.is_kernel_driver_active(interface) is True:
+	  	# tell the kernel to detach
+        	dev.detach_kernel_driver(interface)
+	  	# claim the device
+        	usb.util.claim_interface(dev, interface)
+
+	#md = Mouse_data()
+	#md.x=0.0
+	#md.y=0.0
+	#x=0.0
+	#y=0.0
+	#factor = 0.0000077244
+	#count=25
+	rospy.init_node('mouse_data', anonymous =True)
+	publish = rospy.Publisher('mouse_odom', Mouse_data , queue_size = 10)
+	rate = rospy.Rate(125)
+
+	while not rospy.is_shutdown():
+	        try:
+	                data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
+	                if data[5]>=127:      #IF the mouse is moving in the positiv$
+	                        y=y+(256-data[4])*factor
+	                else:
+	                        y=y-data[4]*factor
+	                if data[3]>=127:      #IF the mouse is moving in the negativ$
+	                        x=x-(256-data[2])*factor
+	                else:
+	                        x=x+(data[2])*factor
+	        except usb.core.USBError as e:
+	                data = None
+	                if e.args == ('Operation timed out',):
+	                        continue
+		rospy.Timer(rospy.Duration(0.2),register_data)
+#	count-=1
+#        if count==0 :
+#		md.x=round(x,5)
+#		md.y=round(y,5)
+#		x=0.0
+#		y=0.0
+#		rospy.loginfo(md)
+#		publish.publish(md)
+#		count=25
+#	rospy.loginfo(md)
+#	publish.publish(md)
+		rate.sleep()
+
+# release the device
+	usb.util.release_interface(dev, interface)
+# reattach the device to the OS kernel
+	dev.attach_kernel_driver(interface)
+if __name__=='__main__':
+        print 'in main'
+	
+
